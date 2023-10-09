fbcf4daada26621323470786f20493c92312811d
EtorArza <etorarza@gmail.com>
2023-10-09 10:56:08 +0200

    add script to genrate all plots

---
+++ scripts/generate_all_plots.sh
@@ -0,0 +1,5 @@
+source venv/bin/activate
+python scripts/experiment_supermario.py --plot
+python scripts/experiment_simulatedARE.py --plot
+python scripts/experiment_veenstra.py --plot
+python scripts/experiment_garage_CMAES_gym.py --plot
